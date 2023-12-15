9ded7ed8c881a21decafa73bb7d1b2aeb9c5fcc9
99cp <103494820+99cp@users.noreply.github.com>
2023-12-15 14:44:26 +0900

    테스트 코드 수정

--- src/test/java/com/example/githubactionstest/GithubactionstestApplicationTests.java
+++ src/test/java/com/example/githubactionstest/GithubactionstestApplicationTests.java
@@ -8,11 +8,12 @@ class GithubactionstestApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("성공");
     }
 
     @Test
     void testFail(){
-        throw new RuntimeException("응 실패야 ~ ");
+//        throw new RuntimeException("응 실패야 ~ ");
     }
 
 }
